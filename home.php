3f0503352ccf46e0dfc04491a81642a4ec40cda2
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-19 10:42:18 -0400

    Update home.php

--- home.php
+++ home.php
@@ -5,9 +5,9 @@ Template Name: Home
 
 get_header(); ?>
 
-	<div class="outter2">
+    <div class="outter2">
         <div class="restrictor">
-        	<div class="inner">
+            <div class="inner">
             	<div class="slider">
                 	<?php echo do_shortcode('[meteor_slideshow]'); ?>
                 </div>
